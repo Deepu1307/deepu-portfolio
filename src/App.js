582bd8cbecd1bab2e9422ea380607c280cffa58e
Deepu Singh <deepujee2020@gmail.com>
2022-11-16 08:44:33 +0530

    Added different components folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Header from "./components/header/Header.jsx";
+import Nav from "./components/nav/Nav.jsx";
+import About from "./components/about/About.jsx";
+import Experience from "./components/experience/Experience.jsx";
+import Services from "./components/services/Services.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Testimonials from "./components/testimonials/Testimonial.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
