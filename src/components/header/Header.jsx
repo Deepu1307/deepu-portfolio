e75f27c7bc7ba90eaaa8b47d810597abf7cd8f51
Deepu Singh <deepujee2020@gmail.com>
2022-12-01 20:38:52 +0530

    Added code for header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import CTA from "./CTA.jsx";
+import HeaderSocials from "./HeaderSocial.jsx";
+import ME from "../../assets/me.png";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Deepu Kumar</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
