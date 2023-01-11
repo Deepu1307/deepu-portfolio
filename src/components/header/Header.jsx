aa6285a86196d90ac7dbcc1f93e0d3d3a1457c7a
Deepu Kumar <Deepujee2020@gmail.com>
2023-01-11 08:39:07 +0530

    Added vanta animation over header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react'
 import CTA from "./CTA.jsx";
 import HeaderSocials from "./HeaderSocial.jsx";
 import ME from "../../assets/me.png";
 import "./header.css";
+import DOTS from 'vanta/dist/vanta.dots.min';
+
 
 const Header = () => {
+
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(DOTS({
+        el: myRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,       
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xff2095,
+        backgroundColor: 0xffffff,
+        size: 4.40,
+        showLines: false
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect]);
+
+
   return (
-    <header>
+    <header ref={myRef}>
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Deepu Kumar</h1>
