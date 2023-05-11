d2144a92e56f01902ea44f3b508e7385b9c9223a
Deepu Kumar <Deepujee2020@gmail.com>
2023-05-11 08:44:01 +0530

    Added some images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me2e.jpg";
+import ME from "../../assets/me2.jpg";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
