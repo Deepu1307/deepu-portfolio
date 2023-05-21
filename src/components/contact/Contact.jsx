3c27d8d7bd8fb4edf53c5d971a59741e5ccdd1b8
Deepu Kumar <Deepujee2020@gmail.com>
2023-05-21 16:02:20 +0530

    Design fix

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,55 +1,65 @@
 import React, { useRef } from "react";
-import { MdOutlineMail } from "react-icons/md"
-import { RiMessengerLine } from "react-icons/ri"
-import { BsWhatsapp } from "react-icons/bs"
+import { MdOutlineMail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import emailjs from 'emailjs-com';
 
 import "./contact.css";
 
 const Contact = () => {
-const formData = useRef();
+  const formData = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-const sendEmail = (e) => {
-  e.preventDefault();
-
-  emailjs.sendForm('service_yg2e2fg', 'template_vd6vut9', formData.current, '84mEZW3YCKSdFXXs-')
-    .then((result) => {
-        e.target.reset();
-        console.log(result.text);
-    }, (error) => {
+    emailjs.sendForm('service_yg2e2fg', 'template_vd6vut9', formData.current, '84mEZW3YCKSdFXXs-')
+      .then((result) => {
+        console.log("deepu result", result);
+        if (result.status === 200) {
+          toast.success("Email sent successfully.");
+          e.target.reset();
+        } else {
+          toast.error("Email didn't send successfully, try again.");
+        }
+      }, (error) => {
         console.log(error.text);
-    });
+      });
+  };
+
+
+  return (
+    <>
+      <ToastContainer />
+        <section id="contact">
+          <h5>Get In Touch</h5>
+          <h2>Contact Me</h2>
+          <div className="container contact__container">
+            <div className="contact__options">
+              <article className="contact__option">
+                <MdOutlineMail className="contact__option-icon" />
+                <h4>Email</h4>
+                <h5>Deepujee2020@gmail.com</h5>
+                <a href="mailto:deepujee2020@gmail.com" target="_blank">Send a message</a>
+              </article>
+              <article className="contact__option">
+                <RiMessengerLine className="contact__option-icon" />
+                <h4>Messenger</h4>
+                <h5>Deepu Singh</h5>
+                <a href="https://m.me/deepu13" target="_blank">Send a message</a>
+              </article>
+            </div>
+            {/* FORM */}
+            <form ref={formData} onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder="Your full name" required />
+              <input type="email" name="email" placeholder="Your email" required />
+              <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+              <button type="submit" className="btn btn-primary">Send Message</button>
+            </form>
+          </div>
+        </section>
+      </>
+      );
 };
 
-
-  return <section id="contact">
-    <h5>Get In Touch</h5>
-    <h2>Contact Me</h2>
-    <div className="container contact__container">
-      <div className="contact__options">
-        <article className="contact__option">
-          <MdOutlineMail className="contact__option-icon" />
-          <h4>Email</h4>
-          <h5>Deepujee2020@gmail.com</h5>
-          <a href="mailto:deepujee2020@gmail.com" target="_blank">Send a message</a>
-        </article>
-        <article className="contact__option">
-          <RiMessengerLine className="contact__option-icon" />
-          <h4>Messenger</h4>
-          <h5>Deepu Singh</h5>
-          <a href="https://m.me/deepu13" target="_blank">Send a message</a>
-        </article>       
-      </div>
-      {/* FORM */}
-      <form ref={formData} onSubmit={sendEmail}>
-        <input type="text" name="name" placeholder="Your full name" required />
-        <input type="email" name="email" placeholder="Your email" />
-        <textarea name="message" rows="7" placeholder="Your Message"></textarea>
-        <button type="submit" className="btn btn-primary">Send Message</button>
-      </form>
-    </div>
-  </section>;
-};
-
-export default Contact;
+      export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,32 +3,34 @@ import "./footer.css";
 import { BsLinkedin, BsGithub, BsFacebook, BsInstagram, BsStackOverflow, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  return <footer>
-    <a href="#" alt="logo name" className="footer_logo">Deepu.</a>
-    <ul className="permalinks">
-      <li><a href="#">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#experience">Experience</a></li>
-      <li><a href="#Services">Services</a></li>
-      <li><a href="#projects">Projects</a></li>
-      <li><a href="#testimonials">Testimonials</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
+  return (
+    <footer>
+      <a href="#" alt="logo name" className="footer_logo">Deepu.</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#Services">Services</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
 
-    <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/deepu1307/" target="_blank"><BsLinkedin className="footer__social__icon" /></a>
-      <a href="https://github.com/Deepu1307" target="_blank"><BsGithub /></a>
-      <a href="https://stackoverflow.com/users/14964835/deepu-kumar" target="_blank"><BsStackOverflow /></a>
-      <a href="https://twitter.com/DeepuDeveloper" target="_blank"><BsTwitter /></a>
-      <a href="https://www.instagram.com/deepuk_1307/" target="_blank"><BsInstagram /></a>
-      <a href="https://www.facebook.com/deepu13/" target="_blank"><BsFacebook /></a>
-    </div>
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/deepu1307/" target="_blank"><BsLinkedin className="footer__social__icon" /></a>
+        <a href="https://github.com/Deepu1307" target="_blank"><BsGithub /></a>
+        <a href="https://stackoverflow.com/users/14964835/deepu-kumar" target="_blank"><BsStackOverflow /></a>
+        <a href="https://twitter.com/DeepuDeveloper" target="_blank"><BsTwitter /></a>
+        <a href="https://www.instagram.com/deepuk_1307/" target="_blank"><BsInstagram /></a>
+        <a href="https://www.facebook.com/deepu13/" target="_blank"><BsFacebook /></a>
+      </div>
 
-  <div className="footer__copyright">
-      <small>&copy; Deepu Kumar. All rights reserved. </small>
-      {/* <u><a href="https://github.com/Deepu1307/deepu-portfolio">Source code of this website</a></u> */}
-    </div>
-  </footer>;
+      <div className="footer__copyright">
+        <small>&copy; Deepu Kumar. All rights reserved. </small>
+        {/* <u><a href="https://github.com/Deepu1307/deepu-portfolio">Source code of this website</a></u> */}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
